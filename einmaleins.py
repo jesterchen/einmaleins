3fd791c037f4dd24c4d56404af25ac6780e6482c
jesterchen <github@jesterweb.de>
2017-07-15 14:03:08 +0200

    several unsepcified changes

--- einmaleins.py
+++ einmaleins.py
@@ -3,21 +3,35 @@ import itertools
 from random import shuffle
 
 
-def generate_exercises(LIMIT=10):
+def generate_exercises(limit, num_exercises):
     exercise_list = []
-    for i, j in itertools.product(range(1, LIMIT + 1), range(1, LIMIT + 1)):
+    for i, j in itertools.product(range(1, limit + 1), range(1, limit + 1)):
         exercise_list.append((i, j, (i) * (j)))
-
     shuffle(exercise_list)
-    return exercise_list
+    return exercise_list[0:num_exercises]
+
+
+def get_user_input(question, default_value):
+    choice = ''
+    while choice == '':
+        try:
+            choice = input(question + " ")
+            if choice == '':
+                choice = default_value
+            choice = int(choice)
+        except:
+            choice = ''
+    return choice
 
 
 if __name__ == "__main__":
-    gridsize = 10
+    gridsize = get_user_input("Bis zu welcher Zahl soll das 1*1 gehen? [10]", 10)
+    print("OK, dann geht es bis %s*%s." % (gridsize, gridsize))
+    num_exercises = get_user_input("Wie viele Aufgaben sollen gestellt werden? [%s] " % (gridsize ** 2),
+                                   gridsize ** 2)
+    print("Es werden also %i Aufgaben gestellt." % (num_exercises))
+    exercise_list = generate_exercises(gridsize, num_exercises)
     incorrect_answers = 0
-    exercise_list = generate_exercises(gridsize)
-    num_exercises = len(exercise_list)
-    print("Insgesamt gibt es %s Aufgaben." % (num_exercises))
     started = datetime.datetime.now().replace(microsecond=0)
     while len(exercise_list) > 0:
         if len(exercise_list) % 10 == 0 and len(exercise_list) != num_exercises:
@@ -38,5 +52,5 @@ if __name__ == "__main__":
                 break
     ended = datetime.datetime.now().replace(microsecond=0)
     print("Du hast von %s Aufgaben %s im ersten Anlauf richtig beantwortet." % (
-    num_exercises, num_exercises - incorrect_answers))
+        num_exercises, num_exercises - incorrect_answers))
     print("Insgesamt hast Du %s gebraucht." % (ended - started))
